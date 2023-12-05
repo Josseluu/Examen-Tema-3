3d71d74b9921d6023b8ce492363e9941d80a1c69
Josseluu <jslslpz@myuax.com>
2023-12-05 10:55:06 +0100

    Initial commit

--- Insertar simbolos.cpp
+++ Insertar simbolos.cpp
@@ -16,36 +16,3 @@ public:
     }
 
 
-    int getSymbolValue(const std::string& name) const {
-        std::map<std::string, int>::const_iterator it = symbols.find(name);
-        if (it != symbols.end()) {
-            return it->second;
-        } else {
-            std::cerr << "Error: El símbolo '" << name << "' no está definido." << std::endl;
-            return 0;
-        }
-    }
-
-private:
-    std::map<std::string, int> symbols;
-};
-
-int main() {
-    Environment environment;
-
-
-    environment.insert("x", 10);
-    environment.insert("y", 20);
-
-
-    environment.insert("x", 30);
-
-
-    int valueX = environment.getSymbolValue("x");
-    int valueY = environment.getSymbolValue("y");
-
-    std::cout << "El valor de x es: " << valueX << std::endl;
-    std::cout << "El valor de y es: " << valueY << std::endl;
-
-    return 0;
-}
