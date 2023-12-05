a359f80069c35d804e02052a495a740adbaf5991
Josseluu <jslslpz@myuax.com>
2023-12-05 10:37:07 +0100

    Initial commit

--- Estructura básica.cpp
+++ Estructura básica.cpp
@@ -4,11 +4,22 @@
 
 class Environment {
 public:
-    // Constructor
     Environment() {}
 
-    // Método para agregar un símbolo a la tabla de símbolos
     void addSymbol(const std::string& name, int value) {
         symbols[name] = value;
     }
 
+    int getSymbolValue(const std::string& name) const {
+        auto it = symbols.find(name);
+        if (it != symbols.end()) {
+            return it->second;
+        } else {
+            std::cerr << "Error: El símbolo '" << name << "' no está definido." << std::endl;
+            return 0;
+        }
+    }
+
+private:
+    std::map<std::string, int> symbols; // Tabla de símbolos
+};
